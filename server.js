3e423fc0420422af6735ddf3cad6d691aa79f9ca
jonpfluger <jonpfluger718@gmail.com>
2022-12-01 20:45:32 -0600

    started setting up views

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const { engine } = require('express-handlebars')
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
@@ -19,6 +20,10 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
